e3fe7a6b71f4dc03334f947bab05b37290603ccc
TitouanGris <fanch.gris@gmail.com>
2023-10-10 18:35:53 +0200

    1st prop

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,30 +1,18 @@
-function PokemonCard() {
-
-    let pokemon = pokemonList[0]
-
-    return pokemon &&
-        pokemon.imgSrc ?
-        <figure>
-            <img src={pokemon.imgSrc} alt={pokemon.name} />
-            <figcaption>{pokemon.name}</figcaption>
-        </figure>
-        :
-        <div>
-            <p>???</p>
-            <figcaption>{pokemon.name}</figcaption>
-        </div>
-
+function PokemonCard(props) {
+  console.log(props);
+  let pokemon = props.pokemon;
+  console.log(props);
+  return pokemon && pokemon.imgSrc ? (
+    <figure>
+      <img src={pokemon.imgSrc} alt={pokemon.name} />
+      <figcaption>{pokemon.name}</figcaption>
+    </figure>
+  ) : (
+    <div>
+      <p>???</p>
+      <figcaption>{pokemon.name}</figcaption>
+    </div>
+  );
 }
-
-const pokemonList = [
-    {
-        name: "bulbasaur",
-        imgSrc:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-        name: "mew",
-    },
-];
 
 export default PokemonCard;
