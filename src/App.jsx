24ec5bbd56303aca8d5beb3840cdb147b3aa1d77
TitouanGris <fanch.gris@gmail.com>
2023-10-11 11:48:37 +0200

    First state

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,47 @@
-import PokemonCard from "./components/PokemonCard"
+import PokemonCard from "./components/PokemonCard";
+import { useState } from "react";
 
 function App() {
+
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
   return (
     <div>
-      <PokemonCard pokemon = {pokemonList[0]}/>
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+
+      {pokemonIndex > 0 && (
+        <button onClick={() => setPokemonIndex(pokemonIndex - 1) }>Précédent</button>
+      )}
+      {pokemonIndex < pokemonList.length - 1 && (
+        <button onClick={() => setPokemonIndex(pokemonIndex + 1) } >Suivant</button>
+      )}
     </div>
   );
 }
 
-const pokemonList =  [
-  {
-      name: "bulbasaur",
-      imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+const pokemonList = [
+{
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
-      name: "mew",
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "mew",
   },
 ];
-
-
-
 export default App;
